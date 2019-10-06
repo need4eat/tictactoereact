b82f0d89029f379a15308d4be230daccb553c2d0
Karthik NS <karthik_ns@live.com>
2019-10-05 17:32:39 -0700

    minimal game works

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,9 @@
 import React, { useState } from "react";
 
-const GameState = {
-  NEW_GAME: "New Game",
-  PLAYER_X: "Player X's Turn",
-  PLAYER_O: "Player Y's Turn",
-  GAME_OVER: "Game Over"
-};
-
 export default function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      This is a sample stateful and server-side rendered React application.
-      <br />
-      <br />
-      Here is a button that will track how many times you click it:
-      <br />
-      <br />
-      <button onClick={() => setCount(count + 1)}>{count}</button>
-      <Game />
-    </div>
-  );
+  return <Game />;
 }
 
 function Game() {
@@ -29,31 +11,89 @@ function Game() {
     currentPlayer: "X",
     nextPlayer: "O",
     board: [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]],
-    gameState: GameState.NEW_GAME
+    isGameInProgress: true
   });
 
   function isYCompleted(cellY, board) {
-    if (board[cellY][0] == "-") return false;
-    let candidate = board[cellY][0];
-    for (let x = 0; x < 2; ++x) {
-      if (board[cellY][x] != candidate) return false;
+    if (board[cellY][0] == "-") {
+      return false;
     }
+
+    const candidate = board[cellY][0];
+    for (let x = 0; x < 3; ++x) {
+      if (board[cellY][x] != candidate) {
+        return false;
+      }
+    }
+
     return true;
   }
 
   function isXCompleted(cellX, board) {
     if (board[0][cellX] == "-") return false;
-    let candidate = board[0][cellX];
-    for (let y = 0; y < 2; ++y) {
+    const candidate = board[0][cellX];
+    for (let y = 0; y < 3; ++y) {
       if (board[y][cellX] != candidate) return false;
     }
     return true;
   }
 
-  function isGameOver(board) {
-    for (let i = 0; i < 2; ++i) {
-      if (isYCompleted(i, board) || isXCompleted(i, board)) return true;
+  function isTopLeftDiagonalCompleted(board) {
+    if (board[0][0] == "-") {
+      return false;
     }
+
+    const candidate = board[0][0];
+    for (let i = 0; i < 3; ++i) {
+      if (board[i][i] != candidate) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function isTopRightDiagonalCompleted(board) {
+    if (board[0][2] == "-") {
+      return false;
+    }
+
+    const candidate = board[0][2];
+    for (let i = 0; i < 3; ++i) {
+      if (board[i][2 - i] != candidate) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  function hasGameEnded(board) {
+    // Check if somebody won the game
+    for (let i = 0; i < 3; ++i) {
+      if (isYCompleted(i, board) || isXCompleted(i, board)) {
+        return true;
+      }
+    }
+
+    if (
+      isTopLeftDiagonalCompleted(board) ||
+      isTopRightDiagonalCompleted(board)
+    ) {
+      return true;
+    }
+
+    // Check if the game ended in a draw
+    let hasGameEndedInADraw = true;
+    for (let i = 0; i < 3; ++i) {
+      for (let j = 0; j < 3; ++j) {
+        if (board[i][j] == "-") {
+          hasGameEndedInADraw = false;
+        }
+      }
+    }
+
+    return hasGameEndedInADraw;
   }
 
   function setCurrentPlayer(player) {
@@ -61,7 +101,7 @@ function Game() {
       currentPlayer: player,
       nextPlayer: player == "X" ? "O" : "X",
       board: state.board,
-      gameState: player == "X" ? setGameState(GameState.PLAYER_X) : setGameState(GameState.PLAYER_O),
+      isGameInProgress: state.isGameInProgress
     });
   }
 
@@ -69,97 +109,139 @@ function Game() {
     state.board[cellY][cellX] = state.currentPlayer;
   }
 
-  function setGameState(gameState) {
-    state.gameState = gameState;
-  }
-
   function onTileClicked(cellX, cellY) {
-    if (state.board[cellY][cellX] != "-") return;
+    if (!state.isGameInProgress) {
+      return;
+    }
+
+    if (state.board[cellY][cellX] != "-") {
+      return;
+    }
 
     setBoardState(cellX, cellY);
-    setCurrentPlayer(state.nextPlayer);
 
-    if (isGameOver(state.board)) {
-      setGameState(GameState.GAME_OVER);
+    if (hasGameEnded(state.board)) {
+      state.isGameInProgress = false;
     }
+
+    setCurrentPlayer(state.nextPlayer);
   }
 
   return (
     <div>
-      <GameStatus gameState={state.gameState} />
+      <GameStatus isGameInProgress={state.isGameInProgress} />
+      <br />
+      <br />
+      <table border="1">
+        <tr>
+          <td>
+            <Tile
+              cellState={state.board[0][0]}
+              onTileClicked={onTileClicked}
+              cellY={0}
+              cellX={0}
+            />
+          </td>
+          <td>
+            <Tile
+              cellState={state.board[0][1]}
+              onTileClicked={onTileClicked}
+              cellY={0}
+              cellX={1}
+            />
+          </td>
+          <td>
+            <Tile
+              cellState={state.board[0][2]}
+              onTileClicked={onTileClicked}
+              cellY={0}
+              cellX={2}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Tile
+              cellState={state.board[1][0]}
+              onTileClicked={onTileClicked}
+              cellY={1}
+              cellX={0}
+            />
+          </td>
+          <td>
+            <Tile
+              cellState={state.board[1][1]}
+              onTileClicked={onTileClicked}
+              cellY={1}
+              cellX={1}
+            />
+          </td>
+          <td>
+            <Tile
+              cellState={state.board[1][2]}
+              onTileClicked={onTileClicked}
+              cellY={1}
+              cellX={2}
+            />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Tile
+              cellState={state.board[2][0]}
+              onTileClicked={onTileClicked}
+              cellY={2}
+              cellX={0}
+            />
+          </td>
+          <td>
+            <Tile
+              cellState={state.board[2][1]}
+              onTileClicked={onTileClicked}
+              cellY={2}
+              cellX={1}
+            />
+          </td>
+          <td>
+            <Tile
+              cellState={state.board[2][2]}
+              onTileClicked={onTileClicked}
+              cellY={2}
+              cellX={2}
+            />
+          </td>
+        </tr>
+      </table>
+      <br />
+      <br />
       <CurrentPlayer currentPlayer={state.currentPlayer} />
-      <Tile
-        cellState={state.board[0][0]}
-        onTileClicked={onTileClicked}
-        cellY={0}
-        cellX={0}
-      />
-      <Tile
-        cellState={state.board[0][1]}
-        onTileClicked={onTileClicked}
-        cellY={0}
-        cellX={1}
-      />
-      <Tile
-        cellState={state.board[0][2]}
-        onTileClicked={onTileClicked}
-        cellY={0}
-        cellX={2}
-      />
-      <br />
-      <Tile
-        cellState={state.board[1][0]}
-        onTileClicked={onTileClicked}
-        cellY={1}
-        cellX={0}
-      />
-      <Tile
-        cellState={state.board[1][1]}
-        onTileClicked={onTileClicked}
-        cellY={1}
-        cellX={1}
-      />
-      <Tile
-        cellState={state.board[1][2]}
-        onTileClicked={onTileClicked}
-        cellY={1}
-        cellX={2}
-      />
-      <br />
-      <Tile
-        cellState={state.board[2][0]}
-        onTileClicked={onTileClicked}
-        cellY={2}
-        cellX={0}
-      />
-      <Tile
-        cellState={state.board[2][1]}
-        onTileClicked={onTileClicked}
-        cellY={2}
-        cellX={1}
-      />
-      <Tile
-        cellState={state.board[2][2]}
-        onTileClicked={onTileClicked}
-        cellY={2}
-        cellX={2}
-      />
     </div>
   );
 }
 
 function Tile(props) {
+  const cellState = props.cellState == "-" ? "" : props.cellState;
   return (
-    <div onClick={() => props.onTileClicked(props.cellX, props.cellY)}>
-      {props.cellState}
+    <div
+      style={{
+        width: "50px",
+        height: "50px",
+        textAlign: "center"
+      }}
+      onClick={() => props.onTileClicked(props.cellX, props.cellY)}
+    >
+      {cellState}
     </div>
   );
 }
 
 function CurrentPlayer(props) {
-  return <div>{props.currentPlayer}</div>;
+  return <div>Next Turn: {props.currentPlayer}</div>;
 }
 
 function GameStatus(props) {
-  return <div>{props.gameState}</div>;
+  const color = props.isGameInProgress ? "green" : "red";
+  return (
+    <div style={{ color: color, fontSize: "30" }}>&nbsp;&nbsp;&nbsp;⏺</div>
+  );
 }
